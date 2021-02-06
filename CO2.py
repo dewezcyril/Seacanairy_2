d096139e275d293bf8e0a5fcbcd0f886be127551
Cyril Dewez <dewezcyril@gmail.com>
2021-02-06 19:38:22 +0100

    Update descriptions of functions Update syntax mistakes from Gustavo First update for smbus2 libraries (need some adjustment or rollback to smbus1) Creation of README file Move some unused part of code to draft.py

--- CO2.py
+++ CO2.py
@@ -3,88 +3,57 @@ from datetime import date
 from datetime import datetime
 import time
 
-# get the locations of the input on the rpi and ADC
+# get the locations of the input on the rpi and ADC (imported from Lukas code)
 import smbus
 import sys
 import subprocess
 
-# ports location
-from smbus import SMBus
+# smbus2 is the new smbus, allow more than 32 bits writing/reading
+from smbus2 import SMBus, i2c_msg
 
-# address of the CO2 device
+
+# I²C address of the CO2 device
 CO2_address = 0x33
 
 
 #emplacement variable
 bus = SMBus(1)
 
-#copy-pasted from the pdf of Epulse: https://www.epluse.com/en/products/co2-measurement/co2-sensor/ee894/
-
-#constant definitions
-TYPE_LO = 0x11          # ControlByte for reading Sensortype Low-Byte
-TYPE_SUB = 0x21         # ControlByte for reading Sensor-Subtype
-AVAIL_PHYSIC_MES = 0x31 # ControlByte for reading Available physical measurements
-TYPE_HI = 0x41          # ControlByte for reading Sensortype High-Byte
-REQU_MES = 0x71         # ControlByte for reading Statusbyte
-MV_1_LO = 0x81          # ControlByte for reading Measurement value 1 Low-Byte
-MV_1_HI = 0x91          # ControlByte for reading Measurement value 1 High-Byte
-MV_2_LO = 0xA1          # ControlByte for reading Measurement value 2 Low-Byte
-MV_2_HI = 0xB1          # ControlByte for reading Measurement value 2 High-Byte
-MV_3_LO = 0xC1          # ControlByte for reading Measurement value 3 Low-Byte
-MV_3_HI = 0xD1          # ControlByte for reading Measurement value 3 High-Byte
-MV_4_LO = 0xE1          # ControlByte for reading Measurement value 4 Low-Byte
-MV_4_HI = 0xF1          # ControlByte for reading Measurement value 4 High-Byte
-E2_DEVICE_ADR = 0       # Address of E2-slave-Device
-TEMP_RH = 0xE000        # Read the temperature value in 0.01 Kelvin and relative humidity value in 0.01 %
-CO2_PRESS = 0xE027      # Read the averaged CO2 value in 1 ppm, the raw CO2 value in 1 ppm and ambient pressure in 0.1 mbar
-CUSTOM = 0x7154         # Command for measurement time configuration and customer adjustment
-
-# declaration of functions
-# I did a translation of the code of Epulse written in C++.
-# But this was too complicated and the lines below are not usefull anymore
-#fl_E2bus_Read_SensorType(void) = 0 #read Sensortype from E2-Interface
-#fl_E2bus_Read_SensorSubType(void) = abc #read Sensor Subtype from E2-Interface
-#fl_E2bus_Read_AvailablePhysicalMeasurements(void) = abc #read available physical Measurements from E2-Interface
-#fl_E2bus_Read_RH(void) # Read Measurement Value 1 (relativ Humidity [%RH])
-#fl_E2bus_Read_Temp(void) # Read Measurement Value 2 (Temperature [°C])
-#fl_E2bus_Read_pres(void) # Read Measurement Value 3 (Ambient pressure [mbar])
-#fl_E2bus_Read_CO2_MEAN(void) # Read Measurement Value 4 (CO2 MEAN [ppm])
-#unsigned char fl_E2bus_Read_Status(void) #read Statusbyte from E2-Interface
-
-
-
-#constants definition
-#from the documentation
-retrys = 3          # number of read attempts
-DELAY_FACTOR = 2    # delay factor for configuration of interface speed
-
 
 def CO2_request_measurement():
-    # Is equal to reading the status
-    # The sensor make the measurements, calculations and calibration by itself.
+    """
+    Request a new measurement to the sensor if the previous one is older than 10 seconds.
+
+    :return:  1 if status of the sensor is OK
+    """
+
     # In the settings of the sensor, the firmware automatically make measurement every x seconds
     # Sending this STATUS will request a new measurement IF the previous one is older than 10 seconds
-    print("********* SEND STATUS ************")
+    print("Send status execution...")
 
     status = bus.read_byte_data(CO2_address, 0x71)
     print("Status of CO2 sensor is ", status)
 
     if(status == 0):
         print("Status is ok")
-        # SHOULD I ADD A NEW LINE HERE? OR 2 LINES?
+
     return status
 
 
 def CO2_get_RH_T():
-    print("****** GET TEMPERATURE AND RH ******")
+    """
+    Read temperature and relative humidity from the CO2 sensor.
 
-    bus.write_I2C_block_data(CO2_address, 0xE0, 0x00)
+    :return:  List of int[Relative Humidity, Temperature]
+    """
+
+    bus.i2c_msg.write(CO2_address, 0xE0, 0x00)
     # write function should automatically  add a 0 at the end, just after the CO2_Address
     # If not, write on address 0x66 (this include the mandatory 1 of the I²C communication)
 
     time.sleep(0.5)     # Copy-pasted from the code of Lukas
 
-    reading = bus.read_I2C_block_data(CO2_address, 48)
+    reading = bus.i2c_msg.read(CO2_address, 48)
     # 48 is the lenght of the string of bytes to read (6 x 8)
     # I should maybe add the ACK of the Master in the length
     # read function should add a 1 after the CO2_Address
@@ -112,15 +81,20 @@ def CO2_get_RH_T():
 
 
 def CO2_get_CO2_P():
-    print("****** GET CO2 AND PRESSURE ******")
+    """
+    Read the CO2 and pressure from the CO2 sensor.
 
-    bus.write_I2C_block_data(CO2_address, 0xE0, 0x27)
+    :return: List of int[CO2 (average), CO2 (instant measurement), Atmospheric Pressure)
+    """
+    print("Reading of CO2 and pressure...")
+
+    bus.i2c_msg.write(CO2_address, 0xE0, 0x27)
     # write function should automatically  add a 0 at the end, just after the CO2_Address
     # If not, write on address 0x66 (this include the mandatory 1 of the I²C communication)
 
     time.sleep(0.5) # Copy-pasted from the code of Lukas
 
-    reading = bus.read_I2C_block_data(CO2_address, 72)
+    reading = bus.i2c_msg.read(CO2_address, 72)
     # 72 is the lenght of the string of bytes to read (9 x 8)
     # I should maybe add the ACK of the Master in the length
     # read function should add a 1 after the CO2_Address
---
+++ README.md
@@ -0,0 +1,17 @@
+# Description of the different files
+
+* **CO2.py**: retrieving the data from [E+E Elektronik EE894 CO2 sensor](https://www.epluse.com/en/products/co2-measurement/co2-sensor/ee894/)
+* **OPC-N3.py**: code imported from an [online example](https://github.com/JarvisSan22/OPC-N3_python), to retrieve the data from the [Alphasense PM OPC-N3 sensor](http://www.alphasense.com/index.php/products/optical-particle-counter/)
+* **Start_to_tshirp.py**: code to retrieve the voltage of a [4 sensors AFE board from Alphasense](http://www.alphasense.com/index.php/products/support-circuits-air/), measured by a [Pi-16ADC](https://alchemy-power.com/pi-16adc/)
+* _**draft.py**: draft document where ideas and non-used part of code are stored_
+# Link to the official documentation
+### E+E Elektronik EE894 CO2 sensor
+* [Utilising the E2 Interface for EE894 - AN1808-1 (296.53 kb)](https://www.epluse.com/fileadmin/data/product/ee894/Utilising_E2_Interface_EE894_AN1808-1.pdf)
+* [E2 interface specification (223.84 kb)](https://www.epluse.com/fileadmin/data/sw/Specification_E2_Interface.pdf)
+* [Protocol Description I²C (1252.75 kb)](https://www.epluse.com/fileadmin/data/product/ee894/TUG_EE894_I2C.pdf)
+
+# About me
+
+Cyril Dewez,
+21 years old,
+Student in Master year at the Antwerp Maritime Academy (Belgium).
