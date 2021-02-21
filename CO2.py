78d6cd5afa1315f3bc9f55da97d1fa1b5a90e071
Cyril Dewez <dewezcyril@gmail.com>
2021-02-21 19:24:14 +0100

    SMBus update i2c.rdwr

--- CO2.py
+++ CO2.py
@@ -9,16 +9,13 @@ Execution at the end of the functions written above
 
 # get the time
 import time
-from datetime import date
+from datetime import date, datetime
 
 # smbus2 is the new smbus, allow more than 32 bits writing/reading
 from smbus2 import SMBus, i2c_msg
 
 # I²C address of the CO2 device
 CO2_address: int = 0x33
-
-# attempt
-i = 0
 
 # emplacement variable
 bus = SMBus(1)
@@ -58,54 +55,6 @@ logger = logging.getLogger('CO2 sensor')
 
 # --------------------------------------------------------
 
-def sleep():
-    time.sleep(0.5)
-
-
-def read(length):
-    """
-    Easy reading on the CO2 sensor.
-    :param length: int, number of bytes to read
-    :return: reading
-    """
-    log = 'I²C reading process from CO2 sensor'
-    logger.debug(log)
-    try:
-        # msg = i2c_msg.read(CO2_address, length)
-        # bus.i2c_rdwr(msg)
-        # reading = msg
-        # It seems that the function return the reading in the same function as the one between brackets (msg)
-        reading = bus.read_i2c_block_data(CO2_address, length)
-        log = "I²C reading is: " + str(reading)
-        logger.debug(log)
-
-    except:
-        log = "Error while reading data from CO2 sensor on I²C"
-        logger.error(log)
-        return 0
-
-    return reading
-
-
-def write(data):
-    """
-    Write two data only to the CO2 sensor.
-    :param data: list of bytes to write
-    """
-    try:
-        log = "I²C writing process to CO2 sensor"
-        logger.debug(log)
-        msg = i2c_msg.write(CO2_address, data)
-        bus.i2c_rdwr(msg)
-        # bus.write_i2c_block_data(CO2_address, data[0], data[1])
-        sleep()
-
-    except:
-        log = "Error while writing " + str(data) + " via I²C to the CO2 sensor"
-        logger.error(log)
-
-    return
-
 
 def CO2_request_measurement():
     """
@@ -135,173 +84,98 @@ def CO2_request_measurement():
         return 0
 
 
-def CO2_get_RH_T():
+def getRHT():
     """
     Read temperature and relative humidity from the CO2 sensor.
+        CO2_get_RH_T()[0] = relative humidity
+        CO2_get_RH_T()[1] = temperature
 
-    CO2_get_RH_T()[0] = relative humidity
-
-    CO2_get_RH_T()[1] = temperature
-
-    :return:  List of int[Relative Humidity, Temperature]
+    :return:  List of int[Relative Humidity (%RH), Temperature (°C)]
     """
-    log = "Read RH and Temperature from CO2 sensor"
+    log = "Reading RH and Temperature from CO2 sensor"
     logger.debug(log)
 
-    try:
-        write([0xE0, 0x00])
+    write = i2c_msg.write(CO2_address, [0xE0, 0x00])
+    read = i2c_msg.read(CO2_address, 6)
 
-        sleep()  # Copy-pasted from the code of Lukas
+    attempts = 0                # reset trial counter
 
-        reading = read(6)
-        # 6 bytes to read
-        # I should maybe add the ACK of the Master in the length
-        # read function should add a 1 after the CO2_Address
-        # If not, write on address 0x67 (this include the mandatory 0 of the I²C communication)
+    while attempts < 3:
+        try:
+            with SMBus(1) as bus:
+                bus.i2c_rdwr(write, read)
+            break               # break the loop if the try has not failed at the previous line
 
-        # reading << 8 = shift bytes 8 times to the left, equally, add 8 times 0 on the right
-        temperature = ((reading[0] << 8) + reading[1]) / 100 - 273.15
-        relative_humidity = ((reading[2] << 8) + reading[3]) / 100
+        except:
+            log = "Error while reading RH and Temperature from CO2 sensor " + str(attempts) + "/3"
+            logger.critical(log)
+            time.sleep(1)       # if I²C comm fails, wait a little bit before the next reading (this is a general
+                                # recommendation concerning I²C comm)
+            attempts += 1       # increment of attempts
+            pass                # doesn't worth to make further calculations if I²C process fails
 
-        log = "Temperature from CO2 sensor is: " + str(temperature) + " °C"
-        logger.info(log)
-        log = "Relative humidity is: " + str(relative_humidity) + " %RH"
-        logger.info(log)
+    reading = list(read)
+    # reading << 8 = shift bytes 8 times to the left, equally, add 8 times 0 on the right
+    temperature = round(((reading[0] << 8) + reading[1]) / 100 - 273.15, 2)
+    relative_humidity = ((reading[3] << 8) + reading[4]) / 100
 
-        RH_T = [relative_humidity, temperature]  # create a chain of values to be returned by the function
+    log = "Temperature from CO2 sensor is: " + str(temperature) + " °C"
+    logger.info(log)
+    log = "Relative humidity is: " + str(relative_humidity) + " %RH"
+    logger.info(log)
 
-        # checking for temperature
-        check.update(reading[0])
-        check.update(reading[1])
-        checksum = check.hexdigest()
-        log = "CRC8 is: " + str(checksum)
-        logger.debug(log)
+    RH_T = [relative_humidity, temperature]  # create a chain of values to be returned by the function
 
-        if checksum == reading[2]:
-            log = "CRC8 is correct. Temperature transmission is correct"
-            logger.debug(log)
-
-        else:
-            log = "CRC8 check found mistake in the I²C transmission for temperature from CO2 sensor"
-            logger.warning(log)
-            log = "CRC8 from sensor is " + str(reading[2]) + " and CRC8 calculation is " + str(crc8)
-            logger.warning(log)
-
-        check.update(reading[3])
-        check.update(reading[4])
-        checksum = check.hexdigest()
-        log = "CRC8 is: " + str(checksum)
-        logger.debug(log)
-
-        if checksum == reading[5]:
-            log = "CRC8 is correct. Relative humidity transmission is correct"
-            logger.debug(log)
-
-        else:
-            log = "CRC8 check found mistake in the I²C transmission for relative humidity"
-            logger.warning(log)
-            log = "CRC8 from sensor is " + str(reading[5]) + " and CRC8 calculation is " + str(checksum)
-            logger.warning(log)
-
-        return RH_T
-
-    except:
-        log = "Error while reading RH and Temperature from the CO2 sensor"
-        logger.error(log)
+    return RH_T
 
 
-def CO2_get_CO2_P():
+def getCO2P():
     """
     Read the CO2 and pressure from the CO2 sensor.
-
-    CO2_get_CO2_P()[0] = CO2_average
-
-    CO2_get_CO2_P()[1] = CO2_raw
-
-    CO2_get_CO2_P()[2] = pressure
+        CO2_get_CO2_P()[0] = CO2_average
+        CO2_get_CO2_P()[1] = CO2_raw
+        CO2_get_CO2_P()[2] = pressure
 
     :return: List of int[CO2 (average), CO2 (instant measurement), Atmospheric Pressure)
     """
-    log = "Reading of CO2 and pressure"
-    print(log)
+    log = 'Reading of CO2 and pressure'
+    logger.debug(log)
 
-    try:
+    write = i2c_msg.write(CO2_address, [0xE0, 0x27])
+    read = i2c_msg.read(CO2_address, 9)
 
-        write([0xE0, 0x27])
-        # write function should automatically  add a 0 at the end, just after the CO2_Address
-        # If not, write on address 0x66 (this include the mandatory 1 of the I²C communication)
+    attempts = 0                # reset trial counter
 
-        sleep()  # Copy-pasted from the code of Lukas
+    while attempts < 3:
+        try:
+            with SMBus(1) as bus:
+                bus.i2c_rdwr(write, read)
+            break               # break the loop if the try has not failed at the previous line
 
-        reading = read(72)
-        # 72 is the lenght of the string of bytes to read (9 x 8)
-        # I should maybe add the ACK of the Master in the length
-        # read function should add a 1 after the CO2_Address
-        # If not, write on address 0x67 (this include the mandatory 0 of the I²C communication)
+        except:
+            log = "Error while reading CO2 and Pressure from CO2 sensor via I²C " + str(attempts) + "/3"
+            logger.critical(log)
+            time.sleep(1)       # if I²C comm fails, wait a little bit before the next reading (this is a general
+                                # recommendation concerning I²C comm)
+            attempts += 1       # increment of attempts
+            pass                # doesn't worth to make further calculations if I²C process fails
 
-        CO2_average = (reading[0] << 8) + reading[1]
-        logger.info("CO2 average is: ", CO2_average, " ppm")
+    reading = list(read)
 
-        CO2_raw = (reading[3] << 8) + reading[4]
-        logger.info("CO2 instant is: ", CO2_raw, " ppm")
+    CO2_average = (reading[0] << 8) + reading[1]
+    log = "CO2 average is: " + str(CO2_average) + " ppm"
+    logger.info(log)
 
-        # reading << 8 = shift bytes 8 times to the left, equally, add 8 times 0 on the right
-        pressure = (((reading[6]) << 8) + reading[7]) / 10
-        logger.info("Pressure is: ", pressure, " mbar")
+    CO2_raw = (reading[3] << 8) + reading[4]
+    log = "CO2 instant is: "+ str(CO2_raw) + " ppm"
+    logger.info(log)
 
-        CO2_P = [CO2_average, CO2_raw, pressure]  # create a chain of values to be returned by the function
+    # reading << 8 = shift bytes 8 times to the left, equally, add 8 times 0 on the right
+    pressure = (((reading[6]) << 8) + reading[7]) / 10
+    log = "Pressure is: " + str(pressure) + " mbar"
+    logger.info(log)
 
-        # checking for temperature
-        check.update(reading[0])
-        check.update(reading[1])
-        checksum = check.hexdigest()
-        log = "CRC8 is: " + str(checksum)
-        logger.debug(log)
-
-        if checksum == reading[2]:
-            log = "CRC8 is correct. CO2 average transmission is correct"
-            logger.debug(log)
-
-        else:
-            log = "CRC8 check found mistake in the I²C transmission for CO2 average"
-            logger.warning(log)
-            log = "CRC8 from sensor is " + str(reading[2]) + " and CRC8 calculation is " + str(checksum)
-            logger.warning(log)
-
-        check.update(reading[3])
-        check.update(reading[4])
-        checksum = check.hexdigest()
-        log = "CRC8 is: " + str(checksum)
-        logger.debug(log)
-
-        if checksum == reading[5]:
-            log = "CRC8 is correct. CO2 raw transmission is correct"
-            logger.debug(log)
-
-        else:
-            log = "CRC8 check found mistake in the I²C transmission for CO2 raw"
-            logger.warning(log)
-            log = "CRC8 from sensor is " + str(reading[5]) + "  and CRC8 calculation is " + str(checksum)
-            logger.warning(log)
-
-            check.update(reading[6])
-            check.update(reading[7])
-            checksum = check.hexdigest()
-
-            if checksum == reading[8]:
-                log = "CRC8 is correct. Pressure transmission is correct"
-                logger.debug(log)
-
-            else:
-                log = "CRC8 check found mistake in the I²C transmission for Pressure"
-                logger.warning(log)
-                log = "CRC8 from sensor is " + str(reading[8]) + " and CRC8 calculation is " + str(checksum)
-                logger.warning(log)
-
-    except:
-        log = "Error while reading CO2 and Pressure"
-        logger.error(log)
-        CO2_P = [0, 0]
+    CO2_P = [CO2_average, CO2_raw, pressure]  # create a chain of values to be returned by the function
 
     return CO2_P
 
@@ -310,14 +184,14 @@ def CO2_get_CO2_P():
 # Settings
 # ---------------------------------------------------------------------
 
-def time_interval(sec):
+def time_interval(seconds):
     """
     Def measurement timestamp for CO2 sensor
-    :param timestamp: seconds
+    :param seconds: seconds
     :return: 0 for success, 1 for error
     """
     try:
-        t = hex(sec * 10)
+        t = hex(seconds * 10)
 
         check.update(0x71)
         check.update(0x54)
@@ -331,7 +205,7 @@ def time_interval(sec):
         sleep()
         reading = read(16)
         set = (reading[0] << 8 + reading[1]) / 10
-        if int(set) == sec:
+        if int(set) == seconds:
             log = "Measurement timestamp set successfully on " + str(set) + " seconds"
             logger.info(log)
             return 0
@@ -401,7 +275,8 @@ log = "Launching a new execution on the " + str(now.strftime("%d/%m/%Y %H:%M:%S"
 logging.info(log)
 
 while (True):
-    CO2_get_RH_T()
-    CO2_get_CO2_P()
+    getRHT()
+    #time.sleep(1)
+    getCO2P()
     print("waiting...")
     time.sleep(20)  # wait 20 seconds
