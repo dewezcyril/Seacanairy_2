9c46e22ca8d581f74d61bc9a4b067fa1bdb88026
Cyril Dewez <dewezcyril@gmail.com>
2021-03-15 15:19:49 +0100

    arduino inspiration

--- OPCN3-myown.py
+++ OPCN3-myown.py
@@ -44,13 +44,52 @@ bus = 0  # name of the SPI bus on the Raspberry Pi 3B+
 device = 0  # name of the SS (Ship Selection) pin used for the OPC-N3
 spi = spidev.SpiDev()  # enable SPI
 spi.open(bus, device)
-spi.max_speed_hz = 1000000  # 750 kHz
+spi.max_speed_hz = 300000  # 750 kHz
 spi.mode = 0b01
 # first bit (from right) = CPHA = 0 --> data are valid when clock is rising
 # seco,d bit (from right) = CPOL = 0 --> clock is kept low when idle
 
 delay_after_command_byte = 0.015  # 15 ms (> 10 ms and < 100 ms)
 delay_between_communications = 0.000015  # 15 mus (> 10 mus and < 100 mus)
+
+# ----------------------------------------------
+# OPC-N3 variables
+# ----------------------------------------------
+SPI_OPC_ready = [0xF3]
+SPI_OPC_busy = [0x31]
+
+
+# ----------------------------------------------
+
+def GetReadyResponse(SPIcommand):
+    """Check that the OPC-N3 is ready to communicate via SPI"""
+    response = spi.xfer(SPIcommand)  # try reading a byte to clear out anything remnant of SD Card activity
+    time.sleep(0.001)  # wait 1 ms
+    print("First response is", response)
+    tries = 0
+    while (response != SPI_OPC_ready):
+        while (tries < 20) and (response != SPI_OPC_ready):
+            response = spi.xfer(SPIcommand)
+            print(response, end='')
+            if response != SPI_OPC_ready:
+                time.sleep(0.001)  # wait 1 ms
+            tries += 1
+
+        if response != SPI_OPC_ready:
+            if response == SPI_OPC_busy:
+                print("OPC-N3 busy, waiting 2 seconds")
+                time.sleep(2)
+            else:
+                spi.close()
+                time.sleep(6)
+                spi.open(bus, device)
+
+        if tries >= 20:
+            print("failed")
+            break
+
+    time.sleep(0.01)  # wait 10 ms
+    print("OPC is ready")
 
 
 def initiate_power_state_control():
@@ -88,9 +127,10 @@ def initiate_power_state_control():
             logger.critical(log)
             return False
 
-answer = spi.readbytes(10)
 
-print(answer)
+GetReadyResponse([0x03])
+
+
 # if initiate_power_state_control():
 #     print("Power state initiation succeeded")
 # else:
