f68d3d1fa941e39a93d335a5fa1d84449f28ffba
Cyril Dewez <dewezcyril@gmail.com>
2021-03-20 17:27:33 +0100

    RPi debugging, deleted unused parts

--- OPCN3.py
+++ OPCN3.py
@@ -1,8 +1,5 @@
 """
-Library for the use and operation of Alphasense OPC-N3 sensor.
-Inspired and adapted from the library of JarvisSan22 available on Github: https://github.com/JarvisSan22/OPC-N3_python
-
-Submitted to LICENCE due to the Git Clone
+Library for the use and operation of Alphasense OPC-N3 sensor
 """
 
 import spidev
@@ -11,6 +8,7 @@ import struct
 import datetime
 import sys
 import os.path
+from progress.bar import IncrementalBar
 # import RPi.GPIO as GPIO
 
 # --------------------------------------------------------
@@ -18,15 +16,26 @@ import os.path
 # --------------------------------------------------------
 import logging
 
-log_file = '/home/pi/seacanairy_project/log/seacanairy.log'
 
 if __name__ == '__main__':
-    message_level = logging.DEBUG
     # If you run the code from this file directly, it will show all the DEBUG messages
+    message_level = logging.DEBUG
+    log_file = '/home/pi/seacanairy_project/log/OPCN3-debug.log'
 
 else:
-    message_level = logging.INFO
     # If you run this code from another file (using this one as a library), it will only print INFO messages
+    message_level = logging.INFO
+    log_file = '/home/pi/seacanairy_project/log/seacanairy.log'
+    # define a Handler which writes INFO messages or higher to the sys.stderr/display
+    console = logging.StreamHandler()
+    console.setLevel(message_level)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    # tell the handler to use this format
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger().addHandler(console)
+
 
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=message_level,
@@ -34,15 +43,6 @@ logging.basicConfig(level=message_level,
                     datefmt='%m-%d %H:%M:%S',
                     filename=log_file,
                     filemode='a')
-# define a Handler which writes INFO messages or higher to the sys.stderr/display
-console = logging.StreamHandler()
-console.setLevel(message_level)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger().addHandler(console)
 
 logger = logging.getLogger('OPC-N3')
 
@@ -114,6 +114,7 @@ def initiate_transmission(command_byte):
             return True  # if function wll continue working once true is returned
 
         if reading == [49]:  # SPI busy = 0x31
+            time.sleep(wait_10_micro)
             attempts += 1
 
         elif reading == [230] or reading == [99] or reading == [0]:
@@ -484,7 +485,7 @@ def read_histogram(timestamp):
         if initiate_transmission(0x30):
             spi.xfer([0x30] * 86)
             spi.close()
-            log = "Old histogram in the OPC-N3 deleted, starting a new one"
+            log = "Old histogram in the OPC-N3 deleted, start a new one"
             logger.debug(log)
             break  # histogram measurement has been initiated, proceeding to
         if attempts < 3:
@@ -499,7 +500,12 @@ def read_histogram(timestamp):
 
     delay = timestamp * 2  # you must wait two times the timestamp in order that
     # the sampling time given by the OPC-N3 respects your sampling time wishes
-    time.sleep(delay)  # sampling time of this histogram
+    bar = IncrementalBar('Sampling', max=(2 * delay), suffix='%(elapsed)s/'+ str(delay) + ' seconds')
+    for i in range(0, 2 * delay):
+        time.sleep(0.5)
+        bar.next()
+
+    bar.finish()
 
     attempts = 1  # reset the counter for next measurement
     try:
@@ -531,41 +537,49 @@ def read_histogram(timestamp):
                     PM1 = round(struct.unpack('f', bytes(PM_A))[0], 3)
                     PM25 = round(struct.unpack('f', bytes(PM_B))[0], 3)
                     PM10 = round(struct.unpack('f', bytes(PM_C))[0], 3)
-                    print("PM 1:", PM1, "mg/m3 | PM 2.5:", PM25, "mg/m3 | PM10:", PM10, "mg/m3")
+                    print("PM 1:\t", PM1, "\t mg/m3")
+                    print("PM 2.5:\t", PM25, "\t mg/m3")
+                    print("PM 10:\t", PM10, "\t mg/m3")
 
                     relative_humidity = round(100 * (join_bytes(relative_humidity) / (2 ** 16 - 1)), 2)
                     temperature = round(-45 + 175 * (join_bytes(temperature) / (2 ** 16 - 1)), 2)  # conversion in °C
                     print("Temperature:", temperature, "| Relative Humidity:", relative_humidity)
 
                     sampling_period = join_bytes(sampling_period) / 100
-                    print("Sampling period:", sampling_period, "seconds")
+                    print(" Sampling period:", sampling_period, "seconds")
                     sample_flow_rate = join_bytes(sample_flow_rate) / 100
-                    print("Sampling flow rate:", sample_flow_rate, "ml/s |", round(sample_flow_rate * 60 / 1000, 2),
-                          "L/min")
+                    print(" Sampling flow rate:", sample_flow_rate, "ml/s |", round(sample_flow_rate * 60 / 1000, 2),"L/min")
 
                     reject_count_glitch = join_bytes(reject_count_glitch)
-                    print("Reject count glitch:", reject_count_glitch)
+                    print(" Reject count glitch:", reject_count_glitch)
                     reject_count_longTOF = join_bytes(reject_count_longTOF)
-                    print("Reject count long TOF:", reject_count_longTOF)
+                    print(" Reject count long TOF:", reject_count_longTOF)
                     reject_count_ratio = join_bytes(reject_count_ratio)
-                    print("Reject count ratio:", reject_count_ratio)
+                    print(" Reject count ratio:", reject_count_ratio)
                     reject_count_Out_Of_Range = join_bytes(reject_count_Out_Of_Range)
-                    print("Reject count Out Of Range:", reject_count_Out_Of_Range)
+                    print(" Reject count Out Of Range:", reject_count_Out_Of_Range)
                     fan_rev_count = join_bytes(fan_rev_count)
-                    print("Fan revolutions count:", fan_rev_count)
+                    print(" Fan revolutions count:", fan_rev_count)
                     laser_status = join_bytes(laser_status)
-                    print("Laser status:", laser_status)
+                    print(" Laser status:", laser_status)
 
-                    print("MToF:", MToF)
-
-                    print("Unused bytes:")
+                    print(" Bin number:\t", end='')
                     for i in range(0, 24):
                         x = 2 * i
                         y = x + 1
                         answer = join_bytes(unused[x:y])
-                        print(answer, ", ", end='')
-                    print("")  # so that newt console printing will go to the next line
-                    return
+                        print(answer, "\t", end='')
+                    print("")  # go to next line
+                    print(" MToF:\t\t", end='')
+                    for i in range(0, 4):
+                        print(MToF[i], "\t", end='')
+                    print("")  # go to next line
+
+                    if sampling_period > (timestamp + 0.5):  # we tolerate a difference of 0.5 seconds
+                        log = "Sampling period of the sensor was " + str(round(sampling_period - timestamp, 2)) + " seconds longer than expected"
+                        logger.info(log)
+
+                    return [PM1, PM25, PM10, temperature, relative_humidity]
 
                 else:
                     log = "Checksum is wrong, trying again to read the histogram (" + str(attempts) + "/3)"
@@ -576,12 +590,25 @@ def read_histogram(timestamp):
         if attempts >= 3:
             log = "Checksum was wrong 3 times, skipping this histogram reading"
             logger.critical(log)
-            return
+            return [-255, -255, -255, -255, -255]
 
     except SystemExit or KeyboardInterrupt:  # to stop the laser and the fan in case of error or shutting down the program
+        log = "Stopping Python instance during measurement, stopping laser and fan"
+        logger.info(log)
         laser_off()
         fan_off()
         raise
+
+
+def getdata(flushing_time, sampling_time):
+    data = [-255, -255, -255, -255, -255]
+    if fan_on():
+        time.sleep(flushing_time)
+        if laser_on():
+            data = read_histogram(sampling_time)
+        laser_off()
+    fan_off()
+    return data
 
 
 def join_bytes(list_of_bytes):
@@ -596,197 +623,7 @@ def join_bytes(list_of_bytes):
     return val
 
 
-def RHcon(ans):
-    # ans is  combine_bytes(ans[52],ans[53])
-    RH = 100 * (ans / (2 ** 16 - 1))
-    return RH
-
-
-def Tempcon(ans):
-    # ans is  combine_bytes(ans[52],ans[53])
-    Temp = -45 + 175 * (ans / (2 ** 16 - 1))
-    return Temp
-
-
-def combine_bytes(LSB, MSB):
-    return (MSB << 8) | LSB
-
-
-def Histdata(ans):
-    """
-    Create a dictionary of all the bytes read.
-    :param ans: chain of bytes to proceed
-    :return:Dictionary of all the possible data read
-    """
-
-    data = {}
-    data['Bin 0'] = combine_bytes(ans[0], ans[1])
-    data['Bin 1'] = combine_bytes(ans[2], ans[3])
-    data['Bin 2'] = combine_bytes(ans[4], ans[5])
-    data['Bin 3'] = combine_bytes(ans[6], ans[7])
-    data['Bin 4'] = combine_bytes(ans[8], ans[9])
-    data['Bin 5'] = combine_bytes(ans[10], ans[11])
-    data['Bin 6'] = combine_bytes(ans[12], ans[13])
-    data['Bin 7'] = combine_bytes(ans[14], ans[15])
-    data['Bin 8'] = combine_bytes(ans[16], ans[17])
-    data['Bin 9'] = combine_bytes(ans[18], ans[19])
-    data['Bin 10'] = combine_bytes(ans[20], ans[21])
-    data['Bin 11'] = combine_bytes(ans[22], ans[23])
-    data['Bin 12'] = combine_bytes(ans[24], ans[25])
-    data['Bin 13'] = combine_bytes(ans[26], ans[27])
-    data['Bin 14'] = combine_bytes(ans[28], ans[29])
-    data['Bin 15'] = combine_bytes(ans[30], ans[31])
-    data['Bin 16'] = combine_bytes(ans[32], ans[33])
-    data['Bin 17'] = combine_bytes(ans[34], ans[35])
-    data['Bin 18'] = combine_bytes(ans[36], ans[37])
-    data['Bin 19'] = combine_bytes(ans[38], ans[39])
-    data['Bin 20'] = combine_bytes(ans[40], ans[41])
-    data['Bin 21'] = combine_bytes(ans[42], ans[43])
-    data['Bin 22'] = combine_bytes(ans[44], ans[45])
-    data['Bin 23'] = combine_bytes(ans[46], ans[47])
-    data['Bin 24'] = combine_bytes(ans[48], ans[49])
-    data['period'] = combine_bytes(ans[52], ans[53])
-    data['FlowRate'] = combine_bytes(ans[54], ans[55])
-    data['Temp'] = Tempcon(combine_bytes(ans[56], ans[57]))
-    data['RH'] = RHcon(combine_bytes(ans[58], ans[59]))
-    data['pm1'] = struct.unpack('f', bytes(ans[60:64]))[0]
-    data['pm2.5'] = struct.unpack('f', bytes(ans[64:68]))[0]
-    data['pm10'] = struct.unpack('f', bytes(ans[68:72]))[0]
-    data['Check'] = combine_bytes(ans[84], ans[85])
-
-    #  print(data)
-    return (data)
-
-
-def read_all(port, chunk_size=86):
-    """Read all characters on the serial port of the OPC-N3 and return them."""
-    #    if not port.timeout:
-    #        raise TypeError('Port needs to have a timeout set!')
-
-    read_buffer = b''
-
-    while True:
-        # Read in chunks. Each chunk will wait as long as specified by
-        # timeout. Increase chunk_size to fail quicker
-        byte_chunk = spi.readbytes(size=chunk_size)
-        print("read_all loop")
-        # read_buffer += byte_chunk
-        if not len(byte_chunk) == chunk_size:
-            break
-
-    return read_buffer
-
-
-def filter_data(response):
-    """
-    Get ride of the 0x61 byte response from the hist data, returning just the wanted data
-    :return:Data cleaned of unnecessary bytes
-    """
-    hist_response = []
-    for j, k in enumerate(response):  # Each of the 86 bytes we expect to be returned is prefixed by 0xFF.
-        if ((j + 1) % 2) == 0:  # Throw away 0th, 2nd, 4th, 6th bytes, etc.
-            hist_response.append(k)
-    return hist_response
-
-
-def getData():
-    """
-    Read the 86 bytes of data from the OPC-N3 sensor.
-    :return: List of 3 items
-    """
-    print("Get PM data")
-    T = 0
-
-    while True:
-        # initsiate getData commnad
-        if initiate_transmission(0x32):
-            # write to the OPC
-            for i in range(14):  # Send the whole stream of bytes at once.
-                spi.writebytes([0x61, 0x01])
-                time.sleep(0.00001)
-                # time.sleep(.1)
-            # read the data
-            ans = bytearray(ser.readall())
-            # print("ans=",ans)
-            ans = filter_data(ans)
-            # print("ans=",ans)
-            b1 = ans[0:4]
-            b2 = ans[4:8]
-            b3 = ans[8:12]
-            c1 = struct.unpack('f', bytes(b1))[0]
-            c2 = struct.unpack('f', bytes(b2))[0]
-            c3 = struct.unpack('f', bytes(b3))[0]
-            check = combine_bytes(ans[12], ans[13])
-            print("Check=", check)
-            return [c1, c2, c3]
-
-
-def getHist():
-    """
-    Get Histogram data from OPC-N3.
-    :return: Dictionary containing the data
-    """
-    # OPC N2 method
-    T = 0  # attemt varaible
-
-    while True:
-        log = "get hist"
-        logger.debug(log)
-
-        if initiate_transmission(0x30):
-            log = "Reading Hist data"
-            logger.debug(log)
-
-            for i in range(86):  # Send the whole stream of bytes at once.
-                spi.writebytes([0x61, 0x01])
-                time.sleep(0.000001)
-
-            # ans=bytearray(ser.read(1))
-            #    print("ans=",ans,"len",len(ans))
-            time.sleep(wait_10_micro)  # delay
-            ans = bytearray(ser.readall())
-            print("ans=", ans, "len", len(ans))
-            ans = filter_data(ans)  # get the wanted data bytes
-            # ans=bytearray(test)
-
-            log = "ans = " + str(ans) + ", len = " + str(len(ans))
-            logger.info(log)
-            # print("test=",test,'len',len(test))
-            data = Histdata(ans)
-
-            return data
-
-
-def getmeasurement():
-    """
-    Start the fan_status, the laser, get measurement, stop the laser and stop de fan_status.
-    :return: Measurements
-    """
-    initiate()
-    initiate_transmission()
-    time.sleep(1)
-    fan_on()
-    time.sleep(5)
-    laser_on()
-    for x in range(0, 1):
-        print(getData())
-        time.sleep(1)
-        print(getHist())
-        time.sleep(5)
-    fan_off()
-    time.sleep(.1)
-    laser_off()
-    time.sleep(.1)
-    ser.close()
-    return
-
-
 if __name__ == '__main__':
-    fan_on()
-    time.sleep(2)
-    laser_on()
-
-    read_histogram(5)
-
-    laser_off()
-    fan_off()
+    # The code below runs if you execute this code from this file (you must execute OPC-N3 and not seacanairy)
+    logger.info("Code is running from the OPC-N3 file itself, debug messages shown")
+    getdata(2, 5)
