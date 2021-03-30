ade44aa22d18a2fb5bae2ff2ffd1135d48ec7470
Cyril Dewez <dewezcyril@gmail.com>
2021-03-31 00:05:03 +0200

    except condition for exit during sampling working

--- OPCN3.py
+++ OPCN3.py
@@ -787,7 +787,7 @@ def getdata(flushing_time, sampling_time):
         return to_return
 
     except:  # in case of error AND if user stop the software
-        print("  ")
+        print("  ")  # go to the next line
         logger.info("Python instance has been stopped, shutting laser and fan OFF...")
         laser_off()
         fan_off()
